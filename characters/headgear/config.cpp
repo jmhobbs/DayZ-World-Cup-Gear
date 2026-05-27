118ce7c9851a3b69ee0202da3d4e2a3848f3e255
John Hobbs <john@velvetcache.org>
2026-05-27 15:43:28 -0500

    Helmets

--- characters/headgear/config.cpp
+++ characters/headgear/config.cpp
@@ -33,4 +33,136 @@ class CfgVehicles
       "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_brazil_co.paa"
     };
   };
+
+  class Mich2001Helmet_WorldCup_Canada : Mich2001Helmet
+  {
+    scope=2;
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_canada_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_canada_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_canada_co.paa"
+    };
+  };
+
+  class Mich2001Helmet_WorldCup_Denmark : Mich2001Helmet
+  {
+    scope=2;
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_denmark_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_denmark_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_denmark_co.paa"
+    };
+  };
+
+  class Mich2001Helmet_WorldCup_England : Mich2001Helmet
+  {
+    scope=2;
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_england_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_england_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_england_co.paa"
+    };
+  };
+
+  class Mich2001Helmet_WorldCup_France : Mich2001Helmet
+  {
+    scope=2;
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_france_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_france_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_france_co.paa"
+    };
+  };
+
+  class Mich2001Helmet_WorldCup_Ireland : Mich2001Helmet
+  {
+    scope=2;
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_ireland_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_ireland_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_ireland_co.paa"
+    };
+  };
+
+  class Mich2001Helmet_WorldCup_Italy : Mich2001Helmet
+  {
+    scope=2;
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_italy_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_italy_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_italy_co.paa"
+    };
+  };
+
+  class Mich2001Helmet_WorldCup_Portugal : Mich2001Helmet
+  {
+    scope=2;
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_portugal_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_portugal_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_portugal_co.paa"
+    };
+  };
+
+  class Mich2001Helmet_WorldCup_Scotland : Mich2001Helmet
+  {
+    scope=2;
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_scotland_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_scotland_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_scotland_co.paa"
+    };
+  };
+
+  class Mich2001Helmet_WorldCup_SouthAfrica : Mich2001Helmet
+  {
+    scope=2;
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_south_africa_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_south_africa_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_south_africa_co.paa"
+    };
+  };
+
+  class Mich2001Helmet_WorldCup_SouthKorea : Mich2001Helmet
+  {
+    scope=2;
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_south_korea_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_south_korea_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_south_korea_co.paa"
+    };
+  };
+
+  class Mich2001Helmet_WorldCup_Sweden : Mich2001Helmet
+  {
+    scope=2;
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_sweden_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_sweden_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_sweden_co.paa"
+    };
+  };
+
+  class Mich2001Helmet_WorldCup_Wales : Mich2001Helmet
+  {
+    scope=2;
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_wales_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_wales_co.paa",
+      "DayZ_World_Cup_Gear\characters\headgear\data\mich2001_wales_co.paa"
+    };
+  };
 };
