6e996fe26b2734687d4621b27731cc5f9eacd9b9
John Hobbs <john@velvetcache.org>
2026-05-28 11:15:47 -0500

    Pouches and plates

--- characters/vests/config.cpp
+++ characters/vests/config.cpp
@@ -43,4 +43,196 @@ class CfgVehicles
       "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_brazil_co.paa"
     };
   };
+
+  class PlateCarrierVest_WorldCup_Canada: PlateCarrierVest
+  {
+    hiddenSelections[]=
+    {
+      "camoGround",
+      "camoMale",
+      "camoFemale"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_canada_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_canada_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_canada_co.paa"
+    };
+  };
+
+  class PlateCarrierVest_WorldCup_Denmark: PlateCarrierVest
+  {
+    hiddenSelections[]=
+    {
+      "camoGround",
+      "camoMale",
+      "camoFemale"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_denmark_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_denmark_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_denmark_co.paa"
+    };
+  };
+
+  class PlateCarrierVest_WorldCup_England: PlateCarrierVest
+  {
+    hiddenSelections[]=
+    {
+      "camoGround",
+      "camoMale",
+      "camoFemale"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_england_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_england_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_england_co.paa"
+    };
+  };
+
+  class PlateCarrierVest_WorldCup_France: PlateCarrierVest
+  {
+    hiddenSelections[]=
+    {
+      "camoGround",
+      "camoMale",
+      "camoFemale"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_france_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_france_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_france_co.paa"
+    };
+  };
+
+  class PlateCarrierVest_WorldCup_Ireland: PlateCarrierVest
+  {
+    hiddenSelections[]=
+    {
+      "camoGround",
+      "camoMale",
+      "camoFemale"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_ireland_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_ireland_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_ireland_co.paa"
+    };
+  };
+
+  class PlateCarrierVest_WorldCup_Italy: PlateCarrierVest
+  {
+    hiddenSelections[]=
+    {
+      "camoGround",
+      "camoMale",
+      "camoFemale"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_italy_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_italy_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_italy_co.paa"
+    };
+  };
+
+  class PlateCarrierVest_WorldCup_Portugal: PlateCarrierVest
+  {
+    hiddenSelections[]=
+    {
+      "camoGround",
+      "camoMale",
+      "camoFemale"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_portugal_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_portugal_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_portugal_co.paa"
+    };
+  };
+
+  class PlateCarrierVest_WorldCup_Scotland: PlateCarrierVest
+  {
+    hiddenSelections[]=
+    {
+      "camoGround",
+      "camoMale",
+      "camoFemale"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_scotland_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_scotland_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_scotland_co.paa"
+    };
+  };
+
+  class PlateCarrierVest_WorldCup_SouthAfrica: PlateCarrierVest
+  {
+    hiddenSelections[]=
+    {
+      "camoGround",
+      "camoMale",
+      "camoFemale"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_south_africa_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_south_africa_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_south_africa_co.paa"
+    };
+  };
+
+  class PlateCarrierVest_WorldCup_SouthKorea: PlateCarrierVest
+  {
+    hiddenSelections[]=
+    {
+      "camoGround",
+      "camoMale",
+      "camoFemale"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_south_korea_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_south_korea_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_south_korea_co.paa"
+    };
+  };
+
+  class PlateCarrierVest_WorldCup_Sweden: PlateCarrierVest
+  {
+    hiddenSelections[]=
+    {
+      "camoGround",
+      "camoMale",
+      "camoFemale"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_sweden_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_sweden_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_sweden_co.paa"
+    };
+  };
+
+  class PlateCarrierVest_WorldCup_Wales: PlateCarrierVest
+  {
+    hiddenSelections[]=
+    {
+      "camoGround",
+      "camoMale",
+      "camoFemale"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_wales_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_wales_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_wales_co.paa"
+    };
+  };
 };
--- gear/containers/config.cpp
+++ gear/containers/config.cpp
@@ -43,4 +43,172 @@ class CfgVehicles
       "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_brazil_co.paa"
     };
   };
+
+  class PlateCarrierPouches_WorldCup_Canada: PlateCarrierPouches
+  {
+    hiddenSelections[]=
+    {
+      "camoGround"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_canada_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_canada_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_canada_co.paa"
+    };
+  };
+
+  class PlateCarrierPouches_WorldCup_Denmark: PlateCarrierPouches
+  {
+    hiddenSelections[]=
+    {
+      "camoGround"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_denmark_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_denmark_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_denmark_co.paa"
+    };
+  };
+
+  class PlateCarrierPouches_WorldCup_England: PlateCarrierPouches
+  {
+    hiddenSelections[]=
+    {
+      "camoGround"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_england_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_england_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_england_co.paa"
+    };
+  };
+
+  class PlateCarrierPouches_WorldCup_France: PlateCarrierPouches
+  {
+    hiddenSelections[]=
+    {
+      "camoGround"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_france_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_france_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_france_co.paa"
+    };
+  };
+
+  class PlateCarrierPouches_WorldCup_Ireland: PlateCarrierPouches
+  {
+    hiddenSelections[]=
+    {
+      "camoGround"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_ireland_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_ireland_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_ireland_co.paa"
+    };
+  };
+
+  class PlateCarrierPouches_WorldCup_Italy: PlateCarrierPouches
+  {
+    hiddenSelections[]=
+    {
+      "camoGround"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_italy_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_italy_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_italy_co.paa"
+    };
+  };
+
+  class PlateCarrierPouches_WorldCup_Portugal: PlateCarrierPouches
+  {
+    hiddenSelections[]=
+    {
+      "camoGround"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_portugal_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_portugal_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_portugal_co.paa"
+    };
+  };
+
+  class PlateCarrierPouches_WorldCup_Scotland: PlateCarrierPouches
+  {
+    hiddenSelections[]=
+    {
+      "camoGround"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_scotland_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_scotland_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_scotland_co.paa"
+    };
+  };
+
+  class PlateCarrierPouches_WorldCup_SouthAfrica: PlateCarrierPouches
+  {
+    hiddenSelections[]=
+    {
+      "camoGround"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_south_africa_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_south_africa_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_south_africa_co.paa"
+    };
+  };
+
+  class PlateCarrierPouches_WorldCup_SouthKorea: PlateCarrierPouches
+  {
+    hiddenSelections[]=
+    {
+      "camoGround"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_south_korea_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_south_korea_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_south_korea_co.paa"
+    };
+  };
+
+  class PlateCarrierPouches_WorldCup_Sweden: PlateCarrierPouches
+  {
+    hiddenSelections[]=
+    {
+      "camoGround"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_sweden_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_sweden_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_sweden_co.paa"
+    };
+  };
+
+  class PlateCarrierPouches_WorldCup_Wales: PlateCarrierPouches
+  {
+    hiddenSelections[]=
+    {
+      "camoGround"
+    };
+    hiddenSelectionsTextures[]=
+    {
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_wales_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_wales_co.paa",
+      "DayZ_World_Cup_Gear\characters\vests\data\ballisticvest_wales_co.paa"
+    };
+  };
 };
