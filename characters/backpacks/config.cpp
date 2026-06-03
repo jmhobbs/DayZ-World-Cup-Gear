79cea314dccafc36abd387a57bcc089807d5f25b
John Hobbs <john@velvetcache.org>
2026-06-03 12:37:10 -0500

    Assault bag classes

---
+++ characters/backpacks/config.cpp
@@ -0,0 +1,224 @@
+class CfgPatches
+{
+  class DayZ_World_Cup_Gear_Characters_Backpacks
+  {
+    weapons[] = {};
+    requiredVersion = 0.1;
+    requiredAddons[] = {"DZ_Data","DZ_Characters","DZ_Characters_Backpacks"};
+    units[] = {};
+  };
+};
+class CfgVehicles
+{
+  class AssaultBag_ColorBase;
+
+  class AssaultBag_WorldCup_USA: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_USA";
+    color="usa";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_usa_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_usa_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_usa_co.paa"
+		};
+	};
+
+  class AssaultBag_WorldCup_Brazil: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_Brazil";
+    color="brazil";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_brazil_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_brazil_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_brazil_co.paa"
+		};
+	};
+
+  class AssaultBag_WorldCup_Canada: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_Canada";
+    color="canada";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_canada_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_canada_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_canada_co.paa"
+		};
+	};
+
+  class AssaultBag_WorldCup_Denmark: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_Denmark";
+    color="denmark";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_denmark_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_denmark_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_denmark_co.paa"
+		};
+	};
+
+  class AssaultBag_WorldCup_England: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_England";
+    color="england";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_england_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_england_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_england_co.paa"
+		};
+	};
+
+  class AssaultBag_WorldCup_France: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_France";
+    color="france";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_france_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_france_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_france_co.paa"
+		};
+	};
+
+  class AssaultBag_WorldCup_Ireland: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_Ireland";
+    color="ireland";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_ireland_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_ireland_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_ireland_co.paa"
+		};
+	};
+
+  class AssaultBag_WorldCup_Italy: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_Italy";
+    color="italy";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_italy_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_italy_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_italy_co.paa"
+		};
+	};
+
+  class AssaultBag_WorldCup_Portugal: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_Portugal";
+    color="portugal";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_portugal_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_portugal_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_portugal_co.paa"
+		};
+	};
+
+  class AssaultBag_WorldCup_Scotland: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_Scotland";
+    color="scotland";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_scotland_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_scotland_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_scotland_co.paa"
+		};
+	};
+
+  class AssaultBag_WorldCup_SouthAfrica: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_SouthAfrica";
+    color="southAfrica";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_south_africa_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_south_africa_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_south_africa_co.paa"
+		};
+	};
+
+  class AssaultBag_WorldCup_SouthKorea: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_SouthKorea";
+    color="southKorea";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_south_korea_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_south_korea_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_south_korea_co.paa"
+		};
+	};
+
+  class AssaultBag_WorldCup_Sweden: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_Sweden";
+    color="sweden";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_sweden_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_sweden_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_sweden_co.paa"
+		};
+	};
+
+  class AssaultBag_WorldCup_Wales: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_Wales";
+    color="wales";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_wales_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_wales_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_wales_co.paa"
+		};
+	};
+
+  class AssaultBag_WorldCup_Russia: AssaultBag_ColorBase
+	{
+		scope=2;
+    displayName="$STR_DZWCG_assaultbag0_Russia";
+    color="russia";
+		visibilityModifier=0.69999999;
+		hiddenSelectionsTextures[]=
+		{
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_russia_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_russia_co.paa",
+			"DayZ_World_Cup_Gear\characters\backpacks\data\boulder_m53_russia_co.paa"
+		};
+	};
+};
